06b442ba8630d8efece98ca86dfc09fa7947bba1
Ruben Pineda <rpineda.ftp@server.ixiam.lan>
2014-01-17 10:52:47 +0100

    Clean code

--- CRM/ContributeTax/Form/Search.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// class CRM_ContributeTax_Form_Search extends CRM_Contribute_Form_Search {
-
-// 	function buildQuickForm() {
-// 		parent::buildQuickForm($this);
-// 		CRM_ContributeTax_BAO_Query::buildSearchForm($this);
-// 	}
-// }
