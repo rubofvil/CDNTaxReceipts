fa042abfb6674740b45fdd6ab4b2a89616899fa2
Ruben Pineda <rpineda.ftp@server.ixiam.lan>
2014-01-17 11:28:22 +0100

    Add file to create the custom searh in the installation

---
+++ CRM/Cdntaxreceipts/Search/ContactContributionTax.mgd.php
@@ -0,0 +1,15 @@
+<?php 
+return array (
+  0 =>
+  array (
+    'name' => 'CRM_Cdntaxreceipts_Search_ContactContributionTax',
+    'entity' => 'CustomSearch',
+    'params' =>
+    array (
+      'version' => 3,
+      'label' => 'Search contacts with tax generated',
+      'description' => 'Search contacts with tax generated',
+      'class_name' => 'CRM_Cdntaxreceipts_Search_ContactContributionTax',
+    ),
+  ),
+);
